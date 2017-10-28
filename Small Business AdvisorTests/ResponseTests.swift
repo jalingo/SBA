695d93fdd04aebe08bf86160be3e06c682731712
Jimmy Lingo <jalingo@berkeley.edu>
2017-10-28 16:43:15 -0700

    ResponseTests green.
    Haven't pulled mock into main target yet.

--- Small Business AdvisorTests/ResponseTests.swift
+++ Small Business AdvisorTests/ResponseTests.swift
@@ -30,14 +30,28 @@ class ResponseTests: XCTestCase {
     
     func testResponseCanOutputByRandom() { XCTAssertNotNil(mock?.byRandom()) }
     
+    // This uses a triple random comparison, which may on occassion fail through odds fulfillment (or luck 🤡).
     func testResponseByRandomWorks() {
+        guard let firstRandom = mock?.byRandom() else { XCTFail(); return }
+        guard let secondRandom = mock?.byRandom() else { XCTFail(); return }
+        guard let thirdRandom = mock?.byRandom() else { XCTFail(); return }
         
+        XCTAssertNotEqual(firstRandom, secondRandom)
+        XCTAssertNotEqual(firstRandom, thirdRandom)
+        XCTAssertNotEqual(secondRandom, thirdRandom)
     }
     
     func testResponseCanOutputByIndex() { XCTAssertNotNil(mock?.byIndex(of: 0)) }
     
     func testResponseByIndexWorks() {
         
+        for index in 1...105 {
+            let validationBody = TextFactory.produce(for: index)
+            let validationTitle = TipCategoryFactory.produceByIndex(index: index).bold
+
+            let validation = "\(validationTitle)\n\n\(validationBody)"
+            XCTAssert(mock?.byIndex(of: index) == NSAttributedString(string: validation), "Failed @:\(index)")
+        }
     }
 }
 
@@ -53,12 +67,17 @@ struct MockResponse: ResponseAggregator {
     func byRandom() -> NSAttributedString {
         let random = TipFactory.produceByRandom()
         
-        let category = random.category
+        let category = random.category.bold
+        let text = random.text
         
-        return NSAttributedString()
+        return NSAttributedString(string: "\(category)\n\n\(text)")
     }
     
-    func byIndex(of: Int) -> NSAttributedString {
-        return NSAttributedString()
+    func byIndex(of index: Int) -> NSAttributedString {
+        
+        let category = TipCategoryFactory.produceByIndex(index: index).bold
+        let text = TextFactory.produce(for: index)
+
+        return NSAttributedString(string: "\(category)\n\n\(text)")
     }
 }
