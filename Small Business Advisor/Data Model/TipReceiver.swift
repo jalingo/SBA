a004e0e0d9f495930ec5e76e2db52d7fca6d9c24
Jimmy Lingo <jalingo@berkeley.edu>
2017-11-16 00:38:36 -0800

    TIpReceiver (no tests)

---
+++ Small Business Advisor/Data Model/TipReceiver.swift
@@ -0,0 +1,29 @@
+//
+//  TipReceiver.swift
+//  Small Biz Advisor
+//
+//  Created by James Lingo on 11/16/17.
+//  Copyright © 2017 Escape Chaos. All rights reserved.
+//
+
+import Foundation
+import MagicCloud
+
+class TipReceiver: ReceivesRecordable {
+    
+    typealias type = Tip
+    
+    /**
+        This protected property is an array of Tips used by reciever.
+     */
+    var recordables = [type]() {
+        didSet { print("** TipReceiver didSet: \(recordables.count)") }
+    }
+    
+    /**
+        This boolean property allows / prevents changes to `recordables` being stored in
+        the cloud.
+     */
+    var allowComponentsDidSetToUploadDataModel: Bool = false
+
+}
