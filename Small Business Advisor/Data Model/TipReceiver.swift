ee4db91e7b4910fc516c228bbea883e4a2c43bb9
Jimmy Lingo <jalingo@berkeley.edu>
2017-11-17 15:35:06 -0800

    TipReceiver started too early. Saving stubs for version 1.3

--- Small Business Advisor/Data Model/TipReceiver.swift
+++ Small Business Advisor/Data Model/TipReceiver.swift
@@ -8,28 +8,46 @@
 
 import Foundation
 import MagicCloud
+import CloudKit
 
 class TipReceiver: ReceivesRecordable {
-    
     typealias type = Tip
     
-    /**
-        This protected property is an array of Tips used by reciever.
-     */
-    var recordables = [type]() {
+    var recordables = [type]() /*{
         didSet {
             print("** TipReceiver didSet: \(recordables.count)")
             
             guard allowRecordablesDidSetToUploadDataModel else { return }
             
-//            let upload: Upload = Upload(
+            let op = Upload(from: self, to: .publicDB)
+            OperationQueue().addOperation(op)
+            
+            // This resets trigger safety
+            allowRecordablesDidSetToUploadDataModel = false
         }
+    }*/
+
+    
+    func startListening() {
+        setupListener(for: RecordType.entry, change: .firesOnRecordCreation) { }
+        setupListener(for: RecordType.entry, change: .firesOnRecordDeletion)
+        setupListener(for: RecordType.entry, change: .firesOnRecordUpdate)
     }
     
-    /**
-        This boolean property allows / prevents changes to `recordables` being stored in
-        the cloud.
-     */
-    var allowRecordablesDidSetToUploadDataModel: Bool = false
-
+    func stopListening() { }
+    
+    func refresh(completion: OptionalClosure = nil) {
+        recordables = []    // <- Clears out current recordables for batch download.
+        
+        let op = Download(type: RecordType.entry, to: self, from: .privateDB)
+        op.completionBlock = completion
+        OperationQueue().addOperation(op)
+    }
+    
+    init() {
+        refresh()
+        startListening()
+    }
+    
+    deinit { stopListening() }
 }
