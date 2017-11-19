23cb67a18f58fe86f5e9db8e1923f7341e996b89
Hayley McCrory <hayleymccrory@Hayley-McCrorys-MacBook-Air.local>
2017-11-18 23:38:38 -0800

    extracted VoteAbstraction out and copied mock implementation into it's own struct

--- Small Business Advisor/Data Model/Vote.swift
+++ Small Business Advisor/Data Model/Vote.swift
@@ -9,6 +9,8 @@
 import CloudKit
 import MagicCloud
 
+// MARK: Protocol
+
 protocol VoteAbstraction: Recordable {
     
     /// This property stores the direction vote was cast: for (true) or against (false).
@@ -21,3 +23,79 @@ protocol VoteAbstraction: Recordable {
     var constituent: CKReference { get set }
 }
 
+// MARK: - Struct
+
+struct Vote: VoteAbstraction {
+    
+    // MARK: - Properties
+    
+    // MARK: - Properties: VoteAbstraction
+    
+    var isFor = true
+    
+    fileprivate var ballotMeasure = CKRecordID(recordName: "\(-1)")
+    var candidate: CKReference {
+        get { return CKReference(recordID: ballotMeasure, action: .deleteSelf) }
+        set { ballotMeasure = newValue.recordID }
+    }
+    
+    fileprivate var voterID = getCurrentUserRecord()
+    var constituent: CKReference {
+        get {
+            let id: CKRecordID
+            voterID == nil ?
+                (id = CKRecordID(recordName: "Credential Failed")) : (id = voterID!)
+
+            return CKReference(recordID: id, action: .deleteSelf)
+        }
+        set { voterID = newValue.recordID }
+    }
+    
+    // MARK: - Properties: Recordable
+    
+    var recordType: String = RecordType.vote
+    
+    var recordFields: Dictionary<String, CKRecordValue> {
+        get {
+            var dictionary = Dictionary<String, CKRecordValue>()
+            
+            dictionary[RecordKey.appr] = isFor as CKRecordValue
+            dictionary[RecordKey.subj] = candidate as CKRecordValue
+            dictionary[RecordKey.votr] = constituent as CKRecordValue
+            
+            return dictionary
+        }
+        
+        set {
+            if let bool = newValue[RecordKey.appr] as? Bool { isFor = bool }
+            if let ref  = newValue[RecordKey.subj] as? CKReference { candidate = ref }
+            if let ref  = newValue[RecordKey.votr] as? CKReference { constituent = ref }
+        }
+    }
+    
+    var recordID = CKRecordID(recordName: "Vote: \(Date())")
+    
+    // MARK: - Functions
+    
+    init() {
+        self.isFor = true
+        
+        let defaultCandidate = CKRecordID(recordName: "DefaultCandidate")
+        self.candidate = CKReference(recordID: defaultCandidate, action: .deleteSelf)
+        
+        let defaultConstituent = CKRecordID(recordName: "DefaultConstituent")
+        self.constituent = CKReference(recordID: defaultConstituent, action: .deleteSelf)
+    }
+    
+    init(up direction: Bool, candidate subject: CKReference, constituent voter: CKReference) {
+        self.isFor = direction
+        self.candidate = subject
+        self.constituent = voter
+    }
+}
+
+extension Vote: Equatable {
+    static func ==(lhs: Vote, rhs: Vote) -> Bool {
+        return lhs.candidate.isEqual(rhs.candidate) && lhs.constituent.isEqual(rhs.constituent)
+    }
+}
