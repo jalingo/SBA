a4756ece3ef61f63e97b94d1bc237424c1647d63
Jimmy Lingo <jalingo@berkeley.edu>
2017-12-11 15:48:03 -0800

    adding ver 2 tip factory tests

---
+++ Small Business AdvisorTests/Vers 2/_TipFactoryTests.swift
@@ -0,0 +1,81 @@
+//
+//  _TipFactoryTests.swift
+//  Small Biz AdvisorTests
+//
+//  Created by James Lingo on 12/11/17.
+//  Copyright © 2017 Escape Chaos. All rights reserved.
+//
+
+import XCTest
+import MagicCloud
+import CloudKit
+
+class _TipFactoryTests: XCTestCase {
+    
+    // MARK: - Properties
+    
+    var mock: _TipFactoryAbstraction?
+    
+    // MARK: - Functions
+    
+    // MARK: - Functions: XCTestCase
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        mock = nil
+        super.tearDown()
+    }
+    
+    // MARK: - Functions: Unit Tests
+    
+    func testTipFactoryHasCount() { XCTAssertNotNil(mock?.count) }
+    
+    func testTipFactoryCanProduceByRank() { XCTAssertNotNil(mock?.rank(of: 1)) }
+    
+    func testTipFactoryRankStaysInRangeRange() {
+        let belowMin = 0
+        let aboveMax = mock!.count + 1
+
+        // This tests that when a rank below 1 is attempted, first is returned instead.
+        if let outOfLowerRangeTip = mock?.rank(of: belowMin), let lowestTipInRange = mock?.rank(of: 1) {
+            XCTAssert(outOfLowerRangeTip.recordID.recordName == lowestTipInRange.recordID.recordName)
+        } else {
+            XCTFail()
+        }
+        
+        // This tests that when a rank above the last rank is attempted, last is returned instead.
+        if let outOfUpperRangeTip = mock?.rank(of: aboveMax), let highestTipInRange = mock?.rank(of: mock!.count) {
+            XCTAssert(outOfUpperRangeTip.recordID.recordName == highestTipInRange.recordID.recordName)
+        } else {
+            XCTFail()
+        }
+    }
+    
+    func testTipFactoryCanProduceByRandom() { XCTAssertNotNil(mock?.random()) }
+}
+
+protocol _TipFactoryAbstraction {
+    
+    var count: Int { get }
+    
+    func rank(of: Int) -> Tip?
+
+    func random() -> Tip?
+}
+
+struct _TipFactory: _TipFactoryAbstraction {
+    
+    // MARK: - Properties
+    
+    var count: Int { return -1 }
+    
+    // MARK: - Functions
+    
+    func random() -> Tip? { return nil }
+    
+    func rank(of: Int) -> Tip? { return nil }
+}
