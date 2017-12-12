f453dbe56b0da711ee2241ae5b9f409e0fa71792
Jimmy Lingo <jalingo@berkeley.edu>
2017-12-11 23:42:33 -0800

    Flagger + tests added

---
+++ Small Business Advisor/Data Model/Moderation/Flagger.swift
@@ -0,0 +1,28 @@
+//
+//  Flagger.swift
+//  Small Biz Advisor
+//
+//  Created by James Lingo on 12/11/17.
+//  Copyright © 2017 Escape Chaos. All rights reserved.
+//
+
+import MagicCloud
+import CloudKit
+
+// !!
+protocol FlaggerAbstraction {
+    func flags(for id: CKRecordID) -> [Flag]?
+    
+    func post(_ flag: Flag)
+}
+
+class Flagger: MCAnyReceiver<Flag>, FlaggerAbstraction {
+    func flags(for id: CKRecordID) -> [Flag]? {
+        return recordables.filter { $0.recordID.recordName == id.recordName }
+    }
+    
+    func post(_ flag: Flag) {
+        let op = MCUpload([flag], from: self, to: .publicDB)
+        OperationQueue().addOperation(op)
+    }
+}
