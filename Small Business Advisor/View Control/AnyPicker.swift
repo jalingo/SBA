183560e4aeee38adb1bcf456576ad41a6e6e7697
Jimmy Lingo <jalingo@berkeley.edu>
2017-12-13 15:13:29 -0800

    created AnyPicker wrapper for UIPickerView and Pickable protocol

---
+++ Small Business Advisor/View Control/AnyPicker.swift
@@ -0,0 +1,41 @@
+//
+//  AnyPicker.swift
+//  Small Biz Advisor
+//
+//  Created by James Lingo on 12/13/17.
+//  Copyright © 2017 Escape Chaos. All rights reserved.
+//
+
+import Foundation
+import MagicCloud
+
+protocol Pickable: MCRecordable {
+    var title: String { get }
+}
+
+class AnyPicker<T: Pickable>: NSObject, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    // MARK: - Properties
+    
+    var receiver = MCAnyReceiver<T>(db: .publicDB)
+    
+    var selectionFollowUp: PickerBlock
+    
+    // MARK: - Functions
+    
+    // MARK: - Functions: UIPickerViewDataSource
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int { return receiver.recordables.count }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int { return 1 }
+    
+    // MARK: - Functions: UIPickerViewDelegate
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if let followUp  = selectionFollowUp { followUp(receiver.recordables[row]) }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return receiver.recordables[row].title
+    }
+}
---
+++ Small Business AdvisorTests/AnyPickerTests.swift
@@ -0,0 +1,50 @@
+//
+//  AnyPickerTests.swift
+//  Small Biz AdvisorTests
+//
+//  Created by James Lingo on 12/12/17.
+//  Copyright © 2017 Escape Chaos. All rights reserved.
+//
+
+import XCTest
+import UIKit
+import MagicCloud
+
+class AnyPickerTests: XCTestCase {
+    
+    // MARK: - Properties
+    
+    var mock: AnyPicker<Tip>?
+    
+    // MARK: - Functions
+    
+    // MARK: - Functions: XCTestCase
+    
+    override func setUp() {
+        super.setUp()
+        mock = AnyPicker()
+    }
+    
+    override func tearDown() {
+        mock = nil
+        super.tearDown()
+    }
+
+    // MARK: - Functions: Unit Tests
+    
+//    func testAnyPickerHasType() { XCTAssertNotNil(mock) }   // <-- Dependency injected in setUp()
+    
+    func testAnyPickerHasReceiver() { XCTAssertNotNil(mock?.receiver) }
+    
+    func testAnyPickerHasSelectionFollowUp() { XCTAssertNotNil(mock?.selectionFollowUp) }
+    
+    func testAnyPickerIsPickerDelegateAndDataSource() {
+        XCTAssert(mock is UIPickerViewDelegate)
+        XCTAssert(mock is UIPickerViewDataSource)
+    }
+    
+}
+
+extension Tip: Pickable {
+    var title: String { return self.text.string[0...30] }
+}
