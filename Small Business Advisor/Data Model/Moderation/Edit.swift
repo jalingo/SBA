d43e7f03e4375380e4ef56f29f1677a017cd2333
Jimmy Lingo <jalingo@berkeley.edu>
2018-01-22 14:14:22 -0800

    Created and conformed to SuggestedModeration protocol

--- Small Business Advisor/Data Model/Moderation/Edit.swift
+++ Small Business Advisor/Data Model/Moderation/Edit.swift
@@ -9,9 +9,10 @@
 import MagicCloud
 import CloudKit
 
-protocol EditAbstraction: MCRecordable {
+// !!
+protocol EditAbstraction: SuggestedModeration {
     var newText: String? { get set }
-    var newCategory: TipCategory? { get set }
+    var newCategory: String? { get set }
     var tip: CKReference { get set }
 }
 
@@ -23,9 +24,13 @@ struct TipEdit: EditAbstraction {
     
     var newText: String?
     
-    var newCategory: TipCategory?
+    var newCategory: String?
+    
+    // MARK: - Properties: SuggestedModeration
     
     var tip: CKReference
+    
+    var editorEmail: String?
     
     // MARK: - Properties: MCRecordable
     
@@ -38,6 +43,14 @@ struct TipEdit: EditAbstraction {
     // MARK: - Functions: Construction
     
     init() { tip = CKReference(recordID: dummyRec, action: .deleteSelf) }
+    
+    init(newText str: String?, newCategory cat: String?, for editedTip: Tip) {
+        newText = str
+        newCategory = cat
+        tip = CKReference(recordID: editedTip.recordID, action: .deleteSelf)
+        
+        recordID = CKRecordID(recordName: "EDIT: \(editedTip.recordID.recordName)")
+    }
 }
 
 extension TipEdit: MCRecordable {
@@ -49,15 +62,15 @@ extension TipEdit: MCRecordable {
             var dict = [String: CKRecordValue]()
             
             if let txt = newText        { dict[RecordKey.ntxt] = txt as CKRecordValue }
-            if let cat = newCategory    { dict[RecordKey.ncat] = cat.rawValue as CKRecordValue }
+            if let str = newCategory    { dict[RecordKey.ncat] = str as CKRecordValue }
             dict[RecordKey.refs] = tip
             
             return dict
         }
         set {
             if let ref = newValue[RecordKey.refs] as? CKReference { tip = ref }
-            if let txt = newValue[RecordKey.ntxt] as? String { newText = txt }
-            if let num = newValue[RecordKey.ncat] as? NSNumber { newCategory = TipCategory(rawValue: num.intValue) }
+            if let txt = newValue[RecordKey.ntxt] as? String      { newText = txt }
+            if let str = newValue[RecordKey.ncat] as? String      { newCategory = str }
         }
     }
     
@@ -65,4 +78,9 @@ extension TipEdit: MCRecordable {
         get { return _recordID ?? dummyRec }
         set(newValue) { _recordID = newValue }
     }
+}
+
+protocol SuggestedModeration {
+    var editorEmail: String? { get set }
+    var tip: CKReference     { get set }
 }
