e9f722c65312e0dc3f19d1066d8cdc52fd6422c3
Jimmy Lingo <jalingo@berkeley.edu>
2018-01-26 17:07:58 -0800

    comments

--- Small Business Advisor/View Control/Graphic Themes.swift
+++ Small Business Advisor/View Control/Graphic Themes.swift
@@ -9,11 +9,17 @@
 import Foundation
 import UIKit
 
-/// This contains system defaults for NSAttributedString formatting.
+// MARK: Struct
+
+/// This struct contains system defaults for NSAttributedString formatting.
 struct Format {
     
+    // MARK: - Properties
+
+    /// This static constant stores the rgb values for theme color "Escape Chaos Green"
     static let ecGreen = UIColor(red: 0.55, green: 0.78, blue: 0.25, alpha: 1.0)
-    
+
+    /// This static computed property returns a centered mutable paragraph style for attributed strings.
     static var style: NSMutableParagraphStyle {
         let _style = NSMutableParagraphStyle()
         _style.alignment = NSTextAlignment.center
@@ -21,6 +27,7 @@ struct Format {
         return _style
     }
     
+    /// This static computed property returns a standartdized shadow adjustment for attributed strings.
     static var shadow: NSShadow {
         let _shadow = NSShadow()
         
@@ -31,36 +38,42 @@ struct Format {
         return _shadow
     }
     
-    /// This method provides the formatting for attributed strings representing entry text.
+    /// This static computed property returns the formatting representing body text for attributed strings.
     static var bodyText: [NSAttributedStringKey: NSObject] {
-        let formatting = [
+        return [
             NSAttributedStringKey.font :            font(size: 18.0),
             NSAttributedStringKey.foregroundColor:  ecGreen,
             NSAttributedStringKey.paragraphStyle:   style
         ]
-        
-        return formatting
     }
     
-    /// This method provides the formatting for attributed strings representing category titles.
+    /// This static computed property returns the formatting representing category titles for attributed strings.
     static var categoryTitle: [NSAttributedStringKey: NSObject] {
-        let formatting = [
+        return [
             NSAttributedStringKey.font :            font(size: 24.0),
             NSAttributedStringKey.foregroundColor:  ecGreen,
             NSAttributedStringKey.shadow:           shadow,
             NSAttributedStringKey.paragraphStyle:   style
         ]
-        
-        return formatting
     }
     
+    // MARK: - Functions
+    
+    /**
+        This method returns a bold system font at the passed size.
+        - Parameter size: Numerical size of text font.
+     */
     static func font(size: CGFloat) -> UIFont { return UIFont.boldSystemFont(ofSize: size) }
 }
 
 // MARK: - Extension: ButtonEnabler
 
-// Set color themes for this app.
+// Set color themes for this app's buttons.
 extension ButtonEnabler {
+
+    /// This read-only property returns the color updateButton will display to indicate enabled.
     var activeColor: UIColor { return Format.ecGreen }
+    
+    /// This read-only property returns the color updateButton will display to indicate disabled.
     var inactiveColor: UIColor { return .gray }
 }
