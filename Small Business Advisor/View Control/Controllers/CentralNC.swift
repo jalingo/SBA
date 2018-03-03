3e0f823ffa20986470321cee2e37951cebd46f82
Jimmy Lingo <jalingo@berkeley.edu>
2018-03-02 19:08:20 -0800

    rm comments

--- Small Business Advisor/View Control/Controllers/CentralNC.swift
+++ Small Business Advisor/View Control/Controllers/CentralNC.swift
@@ -57,17 +57,6 @@ class CentralNC: UINavigationController {
         super.viewDidLoad()
         setNavigationBarHidden(true, animated: false)
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
 
 extension CentralNC: UINavigationBarDelegate {
