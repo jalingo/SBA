e490797ec6eea25908b50f0f8ef8955aa93790c7
Jimmy Lingo <james@escapechaos.com>
2018-03-08 01:49:17 -0800

    Update ModerationTableViewController.swift

--- Small Business Advisor/View Control/Controllers/ModerationTVC/ModerationTableViewController.swift
+++ Small Business Advisor/View Control/Controllers/ModerationTVC/ModerationTableViewController.swift
@@ -123,6 +123,8 @@ extension ModerationTableViewController {
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int { return suggestions.count }
     
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) { tableView.deselectRow(at: indexPath, animated: true) }
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let suggestion = suggestions[indexPath.row]
         
